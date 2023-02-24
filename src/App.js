5c846c0ef947dbe4bb04ad7ecc041d3b7ce09cc7
RajuKirito <chakaragallaraju.1342001@gmail.com>
2023-02-24 14:00:38 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [url, setUrl] = useState("https://www.parchaa.com/");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hi</h1>
+      <button onClick={() => setUrl("https://www.parchaa.com/")}>Home</button>
+      <button onClick={() => setUrl("https://www.parchaa.com/contact")}>
+        Contact Us
+      </button>
+      <iframe src={url} style={{ width: "100%", height: "90%" }}></iframe>
     </div>
   );
 }
