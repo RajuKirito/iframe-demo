78d5d8bc798fd923757570b17358e5ee4f4945b5
RajuKirito <chakaragallaraju.1342001@gmail.com>
2023-02-24 14:56:50 +0530

    First Version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
+import { Box, Grid, Stack } from "@mui/material";
 import { useState } from "react";
+import "./index.css";
 
 function App() {
-  const [url, setUrl] = useState("https://www.parchaa.com/");
+  const [url, setUrl] = useState("https://parchaa.com/");
   return (
-    <div>
-      <h1>hi</h1>
-      <button onClick={() => setUrl("https://www.parchaa.com/")}>Home</button>
-      <button onClick={() => setUrl("https://www.parchaa.com/contact")}>
-        Contact Us
-      </button>
-      <iframe src={url} style={{ width: "100%", height: "90%" }}></iframe>
+    <div className="container">
+      <div className="row">
+        <div className="col-2">
+          <h1>HMIS</h1>
+          <div>
+            <button onClick={() => setUrl("https://parchaa.com/")}>Home</button>
+          </div>
+          <div>
+            <button onClick={() => setUrl("https://app.parchaa.com/")}>
+              App
+            </button>
+          </div>
+          <div>
+            <button onClick={() => setUrl("https://www.parchaa.com/about")}>
+              About us
+            </button>
+          </div>
+          <button onClick={() => setUrl("https://www.parchaa.com/contact")}>
+            Contact Us
+          </button>
+        </div>
+        <div className="col-10">
+          <iframe
+            src={url}
+            width="100%"
+            height="630vh"
+            style={{
+              style: "border:1px solid black"
+              // width: "100%",
+              // height: "300",
+            }}
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
