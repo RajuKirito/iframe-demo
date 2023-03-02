92d9a84dc9bc556d2feaa122efa888c9adaefc2f
RajuKirito <43929838+RajuKirito@users.noreply.github.com>
2023-03-02 13:55:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import { Box, Grid, Stack } from "@mui/material";
-import { useState } from "react";
-import "./index.css";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+/*
 function App() {
-  const [url, setUrl] = useState("https://parchaa.com/");
+  const [url, setUrl] = useState("https://www.parchaa.com/");
+
+  useEffect(() => {
+    const start = async () => {
+      const link = await axios.get(`${baseUrl}` + "home");
+      setUrl(link.data.url);
+    };
+    start();
+  }, []);
+
+  const handleHome =  () => {
+    setUrl("https://www.parchaa.com/home")
+  };
+  const handleApp =  () => {
+  axios.post("https://app.parchaa.com/version-raju-25-2/api/1.1/wf/login-from-frontend",{phone:"9115096873"}).then(res=>console.log(res.data.response))
+    setUrl("https://app.parchaa.com/doctor_parchaa_index");
+  };
+  const handleAbout =  () => {
+    setUrl("https://www.parchaa.com/about");
+  };
+  const handleContact =  () => {
+    setUrl("https://www.parchaa.com/contact");
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-2">
           <h1>HMIS</h1>
           <div>
-            <button onClick={() => setUrl("https://parchaa.com/")}>Home</button>
+            <button onClick={handleHome}>Home</button>
           </div>
           <div>
-            <button onClick={() => setUrl("https://app.parchaa.com/")}>
-              App
-            </button>
+            <button onClick={handleApp}>Log in</button>
           </div>
           <div>
-            <button onClick={() => setUrl("https://www.parchaa.com/about")}>
-              About us
-            </button>
+            <button onClick={handleAbout}>About us</button>
           </div>
-          <button onClick={() => setUrl("https://www.parchaa.com/contact")}>
-            Contact Us
-          </button>
+          <button onClick={handleContact}>Contact Us</button>
         </div>
         <div className="col-10">
           <iframe
@@ -41,6 +57,36 @@ function App() {
       </div>
     </div>
   );
-}
+}*/
 
+const App = () => {
+  let user_id;
+  axios
+    .post(
+      "https://app.parchaa.com/version-raju-25-2/api/1.1/wf/login-from-frontend",
+      { phone: "9115096873" }
+    )
+    .then((res) => {
+      user_id = res.data.response.user_id;
+      localStorage.setItem("user_id", user_id);
+    });
+
+  window.addEventListener("message", receiveMessage, false);
+  function receiveMessage(event) {
+    // Check that the message is coming from website 2
+    if (
+      event.origin !== "https://app.parchaa.com/version-raju-25-2/login-doctor"
+    )
+      return;
+
+    // Send the local storage data to website 2
+    var data = JSON.stringify(localStorage.getItem("user_id"));
+    event.source.postMessage(data, event.origin);
+  }
+  return (
+    <div>
+      <iframe src="https://app.parchaa.com/version-raju-25-2/login-doctor"></iframe>
+    </div>
+  );
+};
 export default App;
